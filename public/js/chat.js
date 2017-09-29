5bb780aadac5983cc7ed23799675663ab17cd006
Anand Deshpande <anandd512@gmail.com>
2017-09-29 13:02:38 +0000

    minor fix

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,5 +1,5 @@
 // Make connection
-var socket = io.connect('http://localhost:4000');
+var socket = io.connect('http://' + location.host + ':4000');
 
 // Query DOM
 var message = document.getElementById('message'),
